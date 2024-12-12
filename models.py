646882b5eb2f19c155e7b319b430a67c7b04f25b
Filipe Lacerda <filipe.lacerd@ensta-paris.fr>
2024-12-12 09:20:22 +0100

    Add: k-NN model

--- models.py
+++ models.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-from sklearn import linear_model, ensemble
+from sklearn import linear_model, ensemble, neighbors
 
 from xgboost import XGBClassifier
 
@@ -13,11 +13,15 @@ class Model:
     def __init__(self, name: str) -> None:
         self.name = name
 
+        self.model = None
+
     def train(self, X: np.ndarray, y: np.ndarray) -> None:
-        raise NotImplementedError
+        self.model.fit(X, y)
 
     def predict(self, X: np.ndarray) -> Union[float, np.ndarray]:
-        raise NotImplementedError
+        pred = self.model.predict(X)
+
+        return pred if len(pred) > 1 else pred.flatten()[0]
 
 
 class FrequencyClassifier(Model):
@@ -40,28 +44,12 @@ class LogisticRegression(Model):
 
         self.model = linear_model.LogisticRegression(max_iter=1000)
 
-    def train(self, X: np.ndarray, y: np.ndarray) -> None:
-        self.model.fit(X, y)
-
-    def predict(self, X: np.ndarray) -> Union[float, np.ndarray]:
-        pred = self.model.predict(X)
-
-        return pred if len(pred) > 1 else pred.flatten()[0]
-
 
 class RandomForest(Model):
     def __init__(self) -> None:
         super().__init__("RandomForest")
 
         self.model = ensemble.RandomForestClassifier(n_estimators=50, max_depth=10)
-
-    def train(self, X: np.ndarray, y: np.ndarray) -> None:
-        self.model.fit(X, y)
-
-    def predict(self, X: np.ndarray) -> Union[float, np.ndarray]:
-        pred = self.model.predict(X)
-
-        return pred if len(pred) > 1 else pred.flatten()[0]
 
 
 class XGBoost(Model):
@@ -70,13 +58,12 @@ class XGBoost(Model):
 
         self.model = XGBClassifier()
 
-    def train(self, X: np.ndarray, y: np.ndarray) -> None:
-        self.model.fit(X, y)
 
-    def predict(self, X: np.ndarray) -> Union[float, np.ndarray]:
-        pred = self.model.predict(X)
+class NearestNeighbour(Model):
+    def __init__(self) -> None:
+        super().__init__("NearestNeighbour")
 
-        return pred if len(pred) > 1 else pred.flatten()[0]
+        self.model = neighbors.KNeighborsClassifier(n_neighbors=5)
 
 
 class Dense(Model):
